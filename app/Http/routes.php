672821c97910c23cce09a27a587bf10e7ade30f6
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-04-02 10:47:06 -0300

    add login route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,11 +1,14 @@
 <?php
 
-Route::group([ 'prefix' => 'v1' ], function () {
+Route::group([ 'prefix' => 'v1', 'middleware' => 'cors' ], function () {
     $exceptRoutes = [ 'create', 'edit' ];
     
     Route::resource('nurseries', 'NurseriesController', ['except' => $exceptRoutes ]);
 
+    Route::post('users/login', 'UsersController@login');
+
     Route::resource('users', 'UsersController', ['except' => $exceptRoutes ]);
+
     Route::get('users/{users}/nurseries', [
         'as'   => 'v1.users.nurseries',
         'uses' => 'UsersController@nurseries',
