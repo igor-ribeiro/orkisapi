f1fddd7500bce1891d059fdcb06d699fa9e14770
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-06-12 13:55:40 -0300

    create orchids table seeder

---
+++ database/seeds/OrchidTableSeeder.php
@@ -0,0 +1,54 @@
+
+<?php
+
+use OrkisApp\Models\Orchid;
+use Illuminate\Database\Seeder;
+
+class OrchidTableSeeder extends Seeder
+{
+    protected $orchids = [
+        [
+            'name' => 'Orquídea Chuva de Ouro',
+            'scientific_name' => 'Oncidium',
+            'image' => 'http://www.fazfacil.com.br/wp-content/uploads/2012/10/20160531-orquidea-oncidium-chuva-de-ouro.jpg',
+            'origin' => 'México, Brasil e América do Sul',
+            'description' => '["Planta herbácea perene de hábito epífita com altura em torno de 35 cm, de pseudobulbos curtos e ovalados e duas folhas estreitas e flexíveis por bulbo.", "As flores são pequenas, de 1,5 cm x 2,0 cm, reunidas em grande panícula sobre haste de 90 cm, que surgem no final da primavera nos estados do sul do país e duram cerca de 15 dias, conforme a temperatura da época.", "As mais conhecidas são as de flores amarelas. Existem também nas cores brancas, róseas, tigradas, marrons e alaranjadas"]',
+            'instructions' => '[{"title":"Plantio","content":["Seu cultivo é muito simples, em vasos de cerâmica crua, de plástico, placas de coco, mas seu melhor desempenho é quando é fixada a troncos de árvores. Aprecia local à meia sombra e para ripados use cobertura com sombreamento a 50%. É tolerante a frio e calor, resistindo bem a amplitudes térmicas de 3 a 35º C.","Esta orquídea, necessita de umidade, portanto em épocas de seca e calor, regue todos os dias."]},{"title":"Adubação","content":["Para adubação, use adubo foliar ou NPK 10-10-10 diluído em água a partir do inverno e depois da floração para fortificar a planta."]}]',
+        ],
+        [
+            'name' => 'Orquídea Mini-Chocolate',
+            'scientific_name' => '(Oncidium Croesus)',
+            'image' => 'http://www.fazfacil.com.br/wp-content/uploads/2015/07/20150709-oncidium-croesus-exib.jpg',
+            'origin' => 'Brasil, das regiões junto ao litoral',
+            'description' => '["Orquídea de pequeno porte, altura até 10 cm, de folhas estreitas, coriácea e lanceoladas medindo 10 cm.A haste floral tem cerca de 10 cm, com uma até cinco flores. Estas têm 2 cm de diâmetro, sendo as três sépalas e duas das pétalas cor de chocolate.","A pétala diferenciada, chamada de labelo, é amarela com mácula central em vermelho.São perfumadas e surgem durante a primavera, tendo uma duração de cerca de dez dias, conforme a temperatura da região.","Pode ser cultivada em orquidários e dentro de casa em todas as regiões do país."]',
+            'instructions' => '[{"title":"Cultivo","content":["Para cultivar em casa, deveremos colocar as mudas em ambiente com muita luz natural, podendo até ter um pouco de sol pela manhã, evitando o sol da tarde muito intenso e quente.Em ripados o sombreamento deverá ser de 50%.","O tipo de recipiente deve ser pequeno, de cerâmica, plástico ou fibra de coco, com furos para drenagem das regas.O substrato é o especial para orquídeas, composto de pedaços de fibra de coco lavadas e com sfagno seco.","Para adubar, poderá usar a adubação líquida de NPK 4-14-8, cerca de uma colher de chá dissolvida em 200 ml de água na temperatura ambiente, dois meses antes da floração.Também poderá optar pelo adubo líquido comercial, obedecendo as quantidades recomendadas na embalagem.","As regas durante a época quente deverão ser regulares, diminuindo durante o inverno.Quando cultivada dentro de casa cuidar para não esquecer da muda, deixando murchar as folhas."]}]',
+        ],
+        [
+            'name' => 'Orquídea Brasileira Maxilaria',
+            'scientific_name' => '(Maxillaria Rupestris)',
+            'image' => 'http://www.fazfacil.com.br/wp-content/uploads/2015/07/20150709-maxilaria-exib.jpg',
+            'origin' => 'Sul do Brasil',
+            'description' => '["Planta herbácea perene de porte pequeno, altura até 25 cm, com folhas lisas de consistência coriácea flexíveis. A haste floral atinge somente 12 cm, cada uma com uma orquídea bem pequena.","A flor é perfumada com dimensão de 3,5 x 3,3 cm, tem três sépalas longas, amarelas na parte interna e brancas na parte externa com pontuações marrons. As pétalas são pequenas, delicadas, brancas com amarelo na parte interna, também com pontuações. A pétala diferenciada é branca com os órgãos de reprodução amarelos e vermelhos. Floresce do final do inverno até a primavera, com as flores durando cerca de 20 dias."]',
+            'instructions' => '[{"title":"Cultivo","content":["Necessita de sombreamento de 40%, podendo ser cultivada dentro de casa em ambiente iluminado pela luz natural e sol somente pela manhã. O tipo de recipiente deve ser pequeno, de cerâmica, plástico ou fibra de coco, com furos para drenagem das regas.","Usar substrato próprio para orquídeas, de fibra de coco particulada mas não em pó. Realizar a adubação dois meses antes da floração, podendo ser adubo formulação NPK 4-14-8 diluído em água, cerca de uma colher das de chá diluída em 200 ml de água na temperatura ambiente.","Regar somente o substrato. Também poderá optar pelo adubo líquido com aspersor, seguindo as instruções da embalagem. As regas devem ser regulares, mas sem encharcar demais o substrato."]}]',
+        ],
+        [
+            'name' => '',
+            'scientific_name' => 'Stanhopea Oculata',
+            'image' => 'http://www.fazfacil.com.br/wp-content/uploads/2013/11/20131121-orquidea-destaque.jpg',
+            'origin' => 'México, Colômbia, Norte do Brasil',
+            'description' => '["Orquídea de formato muito diferente das conhecidas, causando grande admiração quando florida. O tamanho da planta é de 40 cm ou mais em exemplares mais velhos, com folhas longas de mais de 40 cm, largas e frisadas na cor verde escura. Os pseudobulbos são semelhantes a outros gêneros de orquídeas.","As flores têm cor creme amarelada, pontuações em púrpura e máculas escuras dando a impressão de um inseto.","São perfumadas com acento de baunilha e seu florescimento ocorre do verão ao outono e duram cerca de 5 a 15 dias, conforme a temperatura do local."]',
+            'instructions' => '[{"title":"Cultivo","content":["É necessário uma vaso bem aberto e pendente, é possível confeccionar um de tela de galinheiro e arame. O pendão floral sai por entre o substrato e os vãos da tela, formando grande inflorescência pêndula, muito ornamental. Cultivar em substrato bem leve e particulado para permitir o florescimento"]},{"title":"Adubação","content":["Na primavera dissolver adubo granulado NPK formulação 4-14-8, uma colher de chá em 300 ml de água na temperatura ambiente. Regar o substrato todo, que já deverá estar umedecido.","Após a floração realizar uma adubação orgânica, colocando 100 gramas de esterco de galinha em 1 litro de água, mexendo bem e coando. Colocar de 100 a 200 ml no substrato umedecido, isto dará os elementos orgânicos que a planta tem à disposição na natureza quando em estado selvagem."]}]',
+        ],
+    ];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->orchids as $orchid) {
+            factory(Orchid::class)->create($orchid);
+        }
+    }
+}
