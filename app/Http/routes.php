d0b6e3fbdef78d0ea5ea11829a9d18095fa8777a
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-06-19 16:50:47 -0300

    protect api routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,24 @@
 <?php
 
 Route::group([ 'prefix' => 'v1', 'middleware' => 'cors' ], function () {
-    $exceptRoutes = [ 'create', 'edit' ];
+    Route::group([ 'middleware' => 'auth' ], function () {
+        $exceptRoutes = [ 'create', 'edit' ];
 
-    Route::resource('nurseries', 'NurseriesController', ['except' => $exceptRoutes ]);
-    Route::post('nurseries/{document}/{orchidHash}', 'NurseriesController@addOrchid');
-    Route::get('nurseries/available-to-orchid/{username}/{orchidHash}', 'NurseriesController@getAvailableToOrchid');
+        Route::resource('nurseries', 'NurseriesController', ['except' => $exceptRoutes ]);
+        Route::post('nurseries/{document}/{orchidHash}', 'NurseriesController@addOrchid');
+        Route::get('nurseries/available-to-orchid/{username}/{orchidHash}', 'NurseriesController@getAvailableToOrchid');
+
+        Route::resource('users', 'UsersController', ['except' => $exceptRoutes ]);
+
+        Route::get('users/{users}/nurseries', [
+            'as'   => 'v1.users.nurseries',
+            'uses' => 'UsersController@nurseries',
+        ]);
+
+        Route::resource('orchids', 'OrchidsController', [ 'except' => $exceptRoutes ]);
+        Route::get('orchids/{hash}/{nurseryDocument}', 'OrchidsController@hasNursery');
+    });
 
     Route::post('users/login', 'UsersController@login');
-
-    Route::resource('users', 'UsersController', ['except' => $exceptRoutes ]);
-
-    Route::get('users/{users}/nurseries', [
-        'as'   => 'v1.users.nurseries',
-        'uses' => 'UsersController@nurseries',
-    ]);
-
-    Route::resource('orchids', 'OrchidsController', [ 'except' => $exceptRoutes ]);
-    Route::get('orchids/{hash}/{nurseryDocument}', 'OrchidsController@hasNursery');
+    Route::post('users', 'UsersController@store');
 });
